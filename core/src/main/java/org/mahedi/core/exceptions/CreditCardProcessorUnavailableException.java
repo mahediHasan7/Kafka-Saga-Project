dcece6615980a378a171ea6644ce4c7110f58b8e
Mohammed Mahedi Hasan <mahediHasan7@gmail.com>
2026-01-10 12:41:45 +0800

    add: implement CreditCardProcessorController and exception handling

---
+++ core/src/main/java/org/mahedi/core/exceptions/CreditCardProcessorUnavailableException.java
@@ -0,0 +1,7 @@
+package org.mahedi.core.exceptions;
+
+public class CreditCardProcessorUnavailableException extends RuntimeException {
+    public CreditCardProcessorUnavailableException(Throwable cause) {
+        super(cause);
+    }
+}
---
+++ credit-card-processor-service/src/main/java/org/mahedi/creditcardprocessorservice/controller/CreditCardProcessorController.java
@@ -0,0 +1,23 @@
+package org.mahedi.creditcardprocessorservice.controller;
+
+import jakarta.validation.Valid;
+import org.mahedi.core.dto.CreditCardProcessRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/ccp")
+public class CreditCardProcessorController {
+    private final Logger LOGGER = LoggerFactory.getLogger(CreditCardProcessorController.class);
+
+    @PostMapping("/process")
+    public void processCreditCard(@RequestBody @Valid CreditCardProcessRequest payload) {
+        LOGGER.info("Processing request: {}", payload);
+    }
+
+}
