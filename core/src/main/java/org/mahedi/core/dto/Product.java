3daaffa9ad2470eaf35947c234b4ee49683450c6
Mohammed Mahedi Hasan <mahediHasan7@gmail.com>
2026-01-10 12:37:48 +0800

    add: create Product class with attributes and Lombok annotations

---
+++ core/src/main/java/org/mahedi/core/dto/Product.java
@@ -0,0 +1,20 @@
+package org.mahedi.core.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    private UUID id;
+    private String name;
+    private BigDecimal price;
+    private Integer quantity;
+}
