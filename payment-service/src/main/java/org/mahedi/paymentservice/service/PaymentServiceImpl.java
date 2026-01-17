367fecacb6438ec02d4c92916af73d08301293c1
Mohammed Mahedi Hasan <mahediHasan7@gmail.com>
2026-01-17 15:50:48 +0800

    added error handling for credit card processing in PaymentServiceImpl

--- payment-service/src/main/java/org/mahedi/paymentservice/service/PaymentServiceImpl.java
+++ payment-service/src/main/java/org/mahedi/paymentservice/service/PaymentServiceImpl.java
@@ -1,14 +1,20 @@
 package org.mahedi.paymentservice.service;
 
 import org.mahedi.core.events.OrderCreatedEvent;
+import org.mahedi.core.exceptions.CreditCardProcessorUnavailableException;
 import org.mahedi.paymentservice.dto.Payment;
 import org.mahedi.paymentservice.entity.PaymentEntity;
 import org.mahedi.paymentservice.repository.PaymentRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PaymentServiceImpl implements PaymentService {
     private final PaymentRepository paymentRepository;
     private final CreditCardPaymentService creditCardPaymentService;
+    private Logger LOGGER = LoggerFactory.getLogger(PaymentServiceImpl.class);
 
     public PaymentServiceImpl(PaymentRepository paymentRepository, CreditCardPaymentService creditCardPaymentService) {
         this.paymentRepository = paymentRepository;
@@ -18,11 +24,16 @@ public class PaymentServiceImpl implements PaymentService {
     @Override
     public Payment process(OrderCreatedEvent orderCreatedEvent) {
         String SAMPLE_CREDIT_CARD_NUMBER = "1344-4845-348589";
-        creditCardPaymentService.process(SAMPLE_CREDIT_CARD_NUMBER, orderCreatedEvent.getTotalPrice());
+        try {
+            creditCardPaymentService.process(SAMPLE_CREDIT_CARD_NUMBER, orderCreatedEvent.getTotalAmount());
+        } catch (Exception e) {
+            LOGGER.info("Credit card process issue: {}", e.getMessage());
+            throw new CreditCardProcessorUnavailableException(e);
+        }
 
         PaymentEntity paymentEntity = new PaymentEntity();
         paymentEntity.setOrderId(orderCreatedEvent.getOrderId());
-        paymentEntity.setPaymentAmount(orderCreatedEvent.getTotalPrice());
+        paymentEntity.setPaymentAmount(orderCreatedEvent.getTotalAmount());
 
         PaymentEntity saved = paymentRepository.save(paymentEntity);
 
