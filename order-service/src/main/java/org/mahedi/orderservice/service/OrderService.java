82514c4740e300688dab24c6b442360886e01c17
Mohammed Mahedi Hasan <mahediHasan7@gmail.com>
2026-01-17 15:48:19 +0800

    added changeStatus method

--- order-service/src/main/java/org/mahedi/orderservice/service/OrderService.java
+++ order-service/src/main/java/org/mahedi/orderservice/service/OrderService.java
@@ -1,9 +1,14 @@
 package org.mahedi.orderservice.service;
 
 import org.mahedi.core.dto.Order;
+import org.mahedi.core.types.OrderStatus;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public interface OrderService {
     public Order placeOrder(Order order);
+
+    public Order changeStatus(UUID orderId, OrderStatus orderStatus);
 }
