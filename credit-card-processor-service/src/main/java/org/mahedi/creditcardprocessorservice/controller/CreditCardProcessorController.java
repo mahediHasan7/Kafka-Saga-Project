53a8e8b95ae3d5b4677c6e9225b222509b9be64b
Mohammed Mahedi Hasan <mahediHasan7@gmail.com>
2026-01-17 13:21:19 +0800

    fixed RestController annotation

--- credit-card-processor-service/src/main/java/org/mahedi/creditcardprocessorservice/controller/CreditCardProcessorController.java
+++ credit-card-processor-service/src/main/java/org/mahedi/creditcardprocessorservice/controller/CreditCardProcessorController.java
@@ -4,13 +4,13 @@ import jakarta.validation.Valid;
 import org.mahedi.core.dto.CreditCardProcessRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
-@Controller
+@RestController
 @RequestMapping("/ccp")
 public class CreditCardProcessorController {
     private final Logger LOGGER = LoggerFactory.getLogger(CreditCardProcessorController.class);
